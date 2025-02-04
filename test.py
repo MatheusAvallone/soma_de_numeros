fecab5f0ca7a73594c8fb22c73f8fa983cd0043e
Matheus Avallone <biomatheusavl@gmail.com>
2025-02-04 11:29:49 -0300

    Adicionando nova função de subtração

--- test.py
+++ test.py
@@ -7,3 +7,10 @@ def somarNumero (numero1,numero2):
     return Soma
 
 print (somarNumero(18542,462))
+
+def subtrairNumero (numero1,numero2):
+    Subtrair=numero1-numero2
+    return Subtrair
+
+
+print (subtrairNumero(54842,54215))
